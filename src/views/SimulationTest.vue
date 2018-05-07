93914b9511a7675c21f343b57cf80b69f262bce0
hzx-web <13718547328@163.com>
2018-05-07 17:14:07 +0800

    完成首页及模拟考试组件

--- src/views/SimulationTest.vue
+++ src/views/SimulationTest.vue
@@ -1,5 +1,206 @@
 <template>
-    <div>模拟考试</div>
+  <div class="exam-list-content">
+    <div class="list-btn">
+      <a class="default-sort button">默认</a>
+      <a class="reference-number-sort button">参考人数</a>
+    </div>
+    <ul class="exam-list">
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（一）</a></h3>
+            <p class="paper-info">
+              <span class="text-color">总题 <i class="totalSubject">100</i> 题</span>
+              <span class="text-color">总分 <i class="totalScore">100</i> 分</span>
+              <span class="text-color"><i class="referenceNumber">151</i> 人参考</span>
+              <span class="base-color" title="一般">
+                                            难度 <em class="difficultyContent">★★★☆☆</em>
+                                            <em class="difficulty">一般</em>
+                                        </span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <a href="examination.html" class="test-operation-btn button enterProblem">进入做题</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（二）</a></h3>
+            <p class="paper-info">
+              <span class="text-color quiz-Time">2017-03-06</span>
+              <span class="text-color">总题 <i class="totalSubject">100</i> 题</span>
+              <span class="text-color">对 <i class="rightAnswer">20</i> 题</span>
+              <span class="text-color">错 <i class="errorAnswer">10</i> 题</span>
+              <span class="text-color">未答 <i class="nonAnswer">70</i> 题</span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <sapn class="text-color">未完成</sapn>
+            <a href="examination.html" class="test-operation-btn button continueTest">继续考试</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（三）</a></h3>
+            <p class="paper-info">
+              <span class="text-color quiz-Time">2017-03-06</span>
+              <span class="text-color">总分 <i class="totalScore">100</i> 分</span>
+              <span class="base-color">得分 <i class="examScore">100</i> 分</span>
+              <span class="base-color">用时 <i class="examTime">100</i> 分钟</span>
+              <span class="text-color">全站排名 <i class="ranking">20</i> 名</span>
+              <span class="text-color">全站平均分 <i class="averageScore">60.23</i> 分</span>
+              <span class="text-color"><i class="referenceNumber">151</i> 人参考</span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <sapn class="orange-color checkParsing">查看解析</sapn>
+            <a href="examination.html" class="test-operation-btn button againAnswer">再做一次</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（一）</a></h3>
+            <p class="paper-info">
+              <span class="text-color">总题 <i class="totalSubject">100</i> 题</span>
+              <span class="text-color">总分 <i class="totalScore">100</i> 分</span>
+              <span class="text-color"><i class="referenceNumber">151</i> 人参考</span>
+              <span class="text-color"><i class="referenceNumber">151</i> 人参考</span>
+              <span class="base-color" title="简单">
+                                            难度 <em class="difficultyContent">★☆☆☆☆</em>
+                                            <em class="difficulty">简单</em>
+                                        </span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <a href="examination.html" class="test-operation-btn button enterProblem">进入做题</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（二）</a></h3>
+            <p class="paper-info">
+              <span class="text-color quiz-Time">2017-03-06</span>
+              <span class="text-color">总题 <i class="totalSubject">100</i> 题</span>
+              <span class="text-color">对 <i class="rightAnswer">20</i> 题</span>
+              <span class="text-color">错 <i class="errorAnswer">10</i> 题</span>
+              <span class="text-color">未答 <i class="nonAnswer">70</i> 题</span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <sapn class="text-color">未完成</sapn>
+            <a href="examination.html" class="test-operation-btn button continueTest">继续考试</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（三）</a></h3>
+            <p class="paper-info">
+              <span class="text-color quiz-Time">2017-03-06</span>
+              <span class="text-color">总分 <i class="totalScore">100</i> 分</span>
+              <span class="base-color">得分 <i class="examScore">100</i> 分</span>
+              <span class="base-color">用时 <i class="examTime">100</i> 分钟</span>
+              <span class="text-color">全站排名 <i class="ranking">20</i> 名</span>
+              <span class="text-color">全站平均分 <i class="averageScore">60.23</i> 分</span>
+              <span class="text-color"><i class="referenceNumber">151</i> 人参考</span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <sapn class="orange-color checkParsing">查看解析</sapn>
+            <a href="examination.html" class="test-operation-btn button againAnswer">再做一次</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（一）</a></h3>
+            <p class="paper-info">
+              <span class="text-color">总题 <i class="totalSubject">100</i> 题</span>
+              <span class="text-color">总分 <i class="totalScore">100</i> 分</span>
+              <span class="text-color"><i class="referenceNumber">151</i> 人参考</span>
+              <span class="base-color" title="困难">
+                                            难度 <em class="difficultyContent">★★★★★</em>
+                                            <em class="difficulty">困难</em>
+                                        </span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <a href="examination.html" class="test-operation-btn button enterProblem">进入做题</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（三）</a></h3>
+            <p class="paper-info">
+              <span class="text-color quiz-Time">2017-03-06</span>
+              <span class="text-color">总分 <i class="totalScore">100</i> 分</span>
+              <span class="base-color">得分 <i class="examScore">100</i> 分</span>
+              <span class="base-color">用时 <i class="examTime">100</i> 分钟</span>
+              <span class="text-color">全站排名 <i class="ranking">20</i> 名</span>
+              <span class="text-color">全站平均分 <i class="averageScore">60.23</i> 分</span>
+              <span class="text-color"><i class="referenceNumber">151</i> 人参考</span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <sapn class="orange-color checkParsing">查看解析</sapn>
+            <a href="examination.html" class="test-operation-btn button againAnswer">再做一次</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（一）</a></h3>
+            <p class="paper-info">
+              <span class="text-color">总题 <i class="totalSubject">100</i> 题</span>
+              <span class="text-color">总分 <i class="totalScore">100</i> 分</span>
+              <span class="text-color"><i class="referenceNumber">151</i> 人参考</span>
+              <span class="base-color" title="困难">
+                                            难度 <em class="difficultyContent">★★★★★</em>
+                                            <em class="difficulty">困难</em>
+                                        </span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <a href="examination.html" class="test-operation-btn button enterProblem">进入做题</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="exam-list-item clearfix">
+          <div class="pull-left question-paper">
+            <h3><a href="#" title="" class="mockExamTitle">三级人力资源管理师考试阶段强化卷（二）</a></h3>
+            <p class="paper-info">
+              <span class="text-color quiz-Time">2017-03-06</span>
+              <span class="text-color">总题 <i class="totalSubject">100</i> 题</span>
+              <span class="text-color">对 <i class="rightAnswer">20</i> 题</span>
+              <span class="text-color">错 <i class="errorAnswer">10</i> 题</span>
+              <span class="text-color">未答 <i class="nonAnswer">70</i> 题</span>
+            </p>
+          </div>
+          <div class="pull-right exam-status">
+            <sapn class="text-color">未完成</sapn>
+            <a href="examination.html" class="test-operation-btn button continueTest">继续考试</a>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <div class="see-more text-center">
+      <a class="orange-color">查看更多 <img src="images/see-more.png"></a>
+    </div>
+  </div>
 </template>
 
 <script>
